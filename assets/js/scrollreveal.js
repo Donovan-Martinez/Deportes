9602ee94c333874422d05f6d971b48c2d2fcc385
Donovan_Martinez <ddmartinezcas1311@gmail.com>
2022-05-08 22:24:26 -0500

    Agregar 2 secciones a mi primer pagina con Bootstrap

--- assets/js/scrollreveal.js
+++ assets/js/scrollreveal.js
@@ -1,4 +1,6 @@
 ScrollReveal().reveal('#imagenes',{delay: 500});
 ScrollReveal().reveal('#video',{delay: 500});
 ScrollReveal().reveal('#adoptar',{delay: 500});
+ScrollReveal().reveal('#deportistas', {delay: 500})
+
 
